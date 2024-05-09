a70abdda307b005cbd10430594225c55a82d0638
Learn2manage <learn2manage@gmail.com>
2024-05-09 20:33:32 +0800

    Implement tailwind nativewind in View

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,10 @@
 import { Link } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 
 export default function App() {
     return (
-        <View style={styles.container}>
+        <View className="flex-1 items-center justify-center bg-white">
             <Text>Index screen</Text>
             <StatusBar style="auto" />
             <Link href="/profile" style={{ color: 'blue' }}>
@@ -13,12 +13,3 @@ export default function App() {
         </View>
     );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-});
