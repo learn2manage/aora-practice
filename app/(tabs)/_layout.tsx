a486326277bc81e9db8a695d9fe69d0f74943547
Learn2manage <learn2manage@gmail.com>
2024-05-17 23:28:46 +0800

    Prepare initial tabs page display.  And fix icons.ts cannot find module 'file-name.png' typescript problem.

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,11 +1,51 @@
-import { View, Text } from 'react-native';
-import React from 'react';
+import { View, Text, Image } from 'react-native';
+import { Tabs, Redirect } from 'expo-router';
+import { icons } from '../../constants';
+
+const TabIcon = ({
+    icon,
+    color,
+    name,
+    focused,
+}: {
+    icon: any;
+    color: string;
+    name: string;
+    focused: boolean;
+}) => {
+    return (
+        <View>
+            <Image
+                source={icon}
+                resizeMode="contain"
+                tintColor={color}
+                className="w-6 h-6"
+            />
+        </View>
+    );
+};
 
 const TabsLayout = () => {
     return (
-        <View>
-            <Text>TabsLayout</Text>
-        </View>
+        <>
+            <Tabs>
+                <Tabs.Screen
+                    name="home"
+                    options={{
+                        headerShown: false,
+
+                        tabBarIcon: ({ color, focused }) => (
+                            <TabIcon
+                                icon={icons.home}
+                                color={color}
+                                name="home"
+                                focused={focused}
+                            />
+                        ),
+                    }}
+                />
+            </Tabs>
+        </>
     );
 };
 
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -37,6 +37,7 @@ const RootLayout = () => {
 
     return (
         <Stack>
+            {/* <Stack.Screen name="(tabs)" options={{ headerShown: false }} /> */}
             <Stack.Screen name="index" options={{ headerShown: false }} />
         </Stack>
     );
