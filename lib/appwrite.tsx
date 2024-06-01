7e9b39be05df9687a399ef9b1a921a2692caaf39
Learn2manage <learn2manage@gmail.com>
2024-06-01 15:16:02 +0800

    Adding initial appwrite codes, ids stored in env file.

---
+++ lib/appwrite.tsx
@@ -0,0 +1,9 @@
+export const appwriteConfig = {
+    endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+    platform: process.env.EXPO_PUBLIC_APPWRITE_PLATFORM,
+    projectId: process.env.EXPO_PUBLIC_APPWRITE_PLATFORM,
+    databaseId: process.env.EXPO_PUBLIC_APPWRITE_DATABASEID,
+    userCollectionId: process.env.EXPO_PUBLIC_APPWRITE_USERCOLLECTIONID,
+    videoCollectionId: process.env.EXPO_PUBLIC_APPWRITE_VIDEOCOLLECTIONID,
+    storageId: process.env.EXPO_PUBLIC_APPWRITE_STORAGEID,
+};
